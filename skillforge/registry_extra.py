dfa8379156967721ebb0f52333b50f534c855e02
Asim Awdah <asimawdah.developer@gmail.com>
2026-06-23 05:57:14 +0300

    Add Dart registry extra

--- skillforge/registry_extra.py
+++ skillforge/registry_extra.py
@@ -1,1 +1,14 @@
-EXTRA_REGISTRY = {}
+from __future__ import annotations
+
+from .models import StackDefinition
+
+EXTRA_REGISTRY = {
+    "dart": StackDefinition(
+        id="dart",
+        name="Dart",
+        commands={"Install dependencies": "dart pub get", "Analyze": "dart analyze", "Test": "dart test"},
+        directories=["bin/", "lib/", "test/"],
+        avoid=[".dart_tool/", "build/"],
+        rules=["Follow idiomatic Dart conventions."],
+    ),
+}
